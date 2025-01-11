0fc34099369126b0a1f58373bedd959781209e09
Srikanth <gubbasrikanth2355@gmail.com>
2025-01-11 19:29:03 +0530

    done with add edit and delete todo

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Input, Button, Empty,message, Card } from 'antd';
-import {  DeleteFilled, EditFilled } from '@ant-design/icons';
+import { Input, Button, Empty,message, Card, Modal, Popconfirm, Spin     } from 'antd';
+import {  DeleteFilled, EditFilled , CheckCircleOutlined } from '@ant-design/icons';
 
 function Todo() {
+    const { TextArea } = Input;
     const [todo, setTodo] = useState("")
     const [todolist, setTodolist] = useState([{name:"todo1 todo1 todo1 todo1todo1 todo1 todo1 todo1 todo1 todo1 todo1 todo1todo1 todo1 todo1 todo1 todo1 todo1 todo1 todo1todo1 todo1 todo1 todo1 todo1 todo1 todo1 todo1todo1 todo1 todo1 todo1",id:1},{name:"todo2",id:2}])
     const [messageApi, contextHolder] = message.useMessage();
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [edittodo, setEdittodo] = useState({});
+    const [isloading, setIsloading] = useState(false);
     const actions = (item)=> [
-            <EditFilled key="edit" style={{ color: 'green' ,fontSize:"20px" }} onClick={() => console.log(item,"edit")} />,
-            <DeleteFilled key={"delete"} style={{ color: 'red' ,fontSize:"20px" }} onClick={() => console.log(item,"delete")} />
+            <EditFilled key="edit" style={{ color: 'gray' ,fontSize:"20px" }} onClick={openEditModal(item)} />,
+            <CheckCircleOutlined key="check" style={{ color: 'green' ,fontSize:"20px" }} onClick={handleComplete(item)}  />,
+            <Popconfirm
+                title="Delete Task"
+                description="Are you sure to delete this Todo?"
+                onConfirm={handleDelete(item)}
+                onCancel={()=>{}}
+                okText="Yes"
+                cancelText="No"
+            >
+                <DeleteFilled key="delete" style={{ color: 'red' ,fontSize:"20px" }}  />
+                {/* onClick={openDeleteConfirmPopup(item)} */}
+            </Popconfirm>
     ]
 
     const addTodo = useCallback(() => {
@@ -26,20 +41,79 @@ function Todo() {
             });
         }
     })
+
+    const openEditModal = useCallback((item) => () => {
+        setIsloading(true)
+        setTimeout(() => {
+            setEdittodo((prev) => item) 
+            setIsModalVisible(true)
+            setIsloading(false)
+        }, 1000);
+    })
+
+    const handleComplete = useCallback((item) => () => {
+        let completedtodo = item
+    })
+
+    const handleDelete =useCallback((item) => () => {
+        let todos = todolist
+        let index = todos.findIndex((todo) => todo.id === item.id)
+        if(index == -1){
+            messageApi.open({
+                type: 'error',  
+                content: 'Todo not found',
+            })
+        }else{
+            todos.splice(index, 1)
+            setTodolist((prev) => [...todos])
+            messageApi.open({
+                type: 'success',
+                content: 'Todo Deleted successfully',
+            }) 
+        }
+    })
+
+    const handleEdit = useCallback(() => {
+        let todos = todolist
+        if (edittodo.name == "" || edittodo.name.trim() == "") {
+            messageApi.open({
+                type: 'error',
+                content: 'Please enter a todo',
+            });
+            return 
+        }
+        let index = todos.findIndex((todo) => todo.id === edittodo.id)
+        if(index == -1){
+            messageApi.open({
+                type: 'error',  
+                content: 'Todo not found',
+            })
+        }else{
+            todos.splice(index, 1, edittodo)
+            setTodolist((prev) => todos)
+            messageApi.open({
+                type: 'success',
+                content: 'Todo Edited successfully',
+            })        
+        }
+        setIsModalVisible((prev) => false)
+    })
+    
   return (
     <>
         {contextHolder}
+        <Spin spinning={isloading} tip="Loading..." fullscreen />
         <h1 className='text-center font-medium  sm:text-lg lg:text-3xl'>
-            Todo
+            To-Do List
         </h1>
         <div className='flex justify-center items-center my-2'>
-            <Input placeholder="Enter todo" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <TextArea autoSize placeholder="Enter todo" value={todo} onChange={(e) => setTodo(e.target.value)} />
             <Button className='ml-2' onClick={addTodo}>Add Todo</Button>
         </div>
         {todolist.length == 0 ? (
             <>
                 <div className='my-2 flex justify-center items-center h-[80vh]'>
-                    <Empty description="No todos. Try adding a new todo" className='font-normal text-xl ' />
+                    <Empty description="No to-dos found. Try adding a new one" className='font-normal text-xl ' />
                 </div>
             </>
         ):(
@@ -56,6 +130,13 @@ function Todo() {
                 ))}
             </div>
         )}
+
+        {/* edit modal */}
+        <Modal title="Update Todo" open={isModalVisible} onOk={handleEdit} onCancel={() => setIsModalVisible((prev) => (false))}>
+            <TextArea value={edittodo.name} onChange={(e) => setEdittodo((prev) => ({...prev, name: e.target.value}))} />
+        </Modal>
+
+        
 
 
     </>
